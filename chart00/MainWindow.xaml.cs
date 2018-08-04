64f540b33c96cfdcc1630fb86ceda760662e053e
AkioMaruyama1964 <valg05@icloud.com>
2018-08-04 15:38:58 +0900

    init

---
+++ chart00/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace chart00
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+ 
+
+        public ObservableCollection<Point> Points { get; set; }
+        public ObservableCollection<Point> Points2 { get; set; }
+
+        public MainWindow() {
+            InitializeComponent();
+
+            Points = new ObservableCollection<Point>();
+            Points2 = new ObservableCollection<Point>();
+
+            Points.Add(new Point { X = 0, Y = 0 });
+            Points.Add(new Point { X = 1, Y = 5 });
+            Points.Add(new Point { X = 2, Y = 7 });
+            Points.Add(new Point { X = 3, Y = 14 });
+            Points.Add(new Point { X = 4, Y = 10 });
+            Points.Add(new Point { X = 5, Y = 12 });
+            Points.Add(new Point { X = 6, Y = 7 });
+            Points.Add(new Point { X = 7, Y = 18 });
+            Points.Add(new Point { X = 8, Y = 18 });
+            Points.Add(new Point { X = 9, Y = 10 });
+            Points.Add(new Point { X = 10, Y = 14 });
+
+            Points2.Add(new Point { X = 0, Y = 8 });
+            Points2.Add(new Point { X = 2, Y = 3 });
+            Points2.Add(new Point { X = 6, Y = 10 });
+            Points2.Add(new Point { X = 10, Y = 5 });
+            Points2.Add(new Point { X = 4, Y = 0 });
+
+            this.DataContext = this;
+        }
+    }
+}
---
+++ chart00/Window1.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Controls.DataVisualization.Charting;
+
+
+namespace chart00
+{
+    /// <summary>
+    /// Window1.xaml の相互作用ロジック
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1() {
+            InitializeComponent();
+            showColumnChart();
+
+            line01.DataContext = valueList01;
+            line02.DataContext = valueList02;
+            _chart.Title = "Hoge chart";
+            //line01.DependentRangeAxis.DependentAxes.Max = 500;
+        }
+
+        public List<KeyValuePair<string, int>> valueList01 = new List<KeyValuePair<string, int>>();
+        public List<KeyValuePair<string, int>> valueList02 = new List<KeyValuePair<string, int>>();
+
+        //List<KeyValuePair<string, int>> valueList = new List<KeyValuePair<string, int>>();
+        private void showColumnChart() {
+            //List<KeyValuePair<string, int>> valueList = new List<KeyValuePair<string, int>>();
+            valueList01.Add(new KeyValuePair<string, int>("Jan.", 60));
+            valueList01.Add(new KeyValuePair<string, int>("Feb.", 10));
+            valueList01.Add(new KeyValuePair<string, int>("Mar.", 10));
+            valueList01.Add(new KeyValuePair<string, int>("Apr.", 30));
+            valueList01.Add(new KeyValuePair<string, int>("May",  1));
+            valueList01.Add(new KeyValuePair<string, int>("Jun.", 2));
+            valueList01.Add(new KeyValuePair<string, int>("Jul.", 0));
+            valueList01.Add(new KeyValuePair<string, int>("Aug.", 5));
+            valueList01.Add(new KeyValuePair<string, int>("Sep.", 80));
+            valueList01.Add(new KeyValuePair<string, int>("Oct.", 30));
+            valueList01.Add(new KeyValuePair<string, int>("Nov.", 5));
+            valueList01.Add(new KeyValuePair<string, int>("Dec.", 15));
+
+            valueList02.Add(new KeyValuePair<string, int>("Jan.", 10));
+            valueList02.Add(new KeyValuePair<string, int>("Feb.", 20));
+            valueList02.Add(new KeyValuePair<string, int>("Mar.", 110));
+            valueList02.Add(new KeyValuePair<string, int>("Apr.", 3));
+            valueList02.Add(new KeyValuePair<string, int>("May", 10));
+            valueList02.Add(new KeyValuePair<string, int>("Jun.", 22));
+            valueList02.Add(new KeyValuePair<string, int>("Jul.", 33));
+            valueList02.Add(new KeyValuePair<string, int>("Aug.", 4));
+            valueList02.Add(new KeyValuePair<string, int>("Sep.", 8));
+            valueList02.Add(new KeyValuePair<string, int>("Oct.", 330));
+            valueList02.Add(new KeyValuePair<string, int>("Nov.", 51));
+            valueList02.Add(new KeyValuePair<string, int>("Dec.", 15));
+
+        }
+    }
+}
